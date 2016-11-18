7d0798a4df4a1020d44571ed714edff268ee17c7
QianJin2013 <osexp2003@gmail.com>
2016-11-18 22:39:37 +0900

    Add files via upload

---
+++ GroupPolicyObject2Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupPolicy;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static Guid myGuid = Guid.NewGuid();
+
+        static void Main(string[] args)
+        {
+            IGroupPolicyObject2 gpo = (IGroupPolicyObject2)new GroupPolicyClass();
+            try
+            {
+                gpo.OpenLocalMachineGPO(GroupPolicyFlags.LoadRegistryInformation);
+
+                StringBuilder rootRegPath = new StringBuilder(1024);
+                gpo.GetRegistryKeyPath(GroupPolicySection.User, rootRegPath, rootRegPath.Capacity);
+
+                Console.WriteLine("REG:" + rootRegPath);
+
+                //do your work here, change the registry 
+
+                gpo.Save(/*machine part:*/false, /*extension add:*/true, GroupPolicyExtensionGuids.Registry, myGuid);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
